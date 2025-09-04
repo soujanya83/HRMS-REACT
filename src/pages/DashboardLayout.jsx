ce9291175682adf419994b7a52b074fa21d7ae83
Dilnawazkhan302 <react@fraxxratech.com>
2025-09-04 09:51:16 +0530

    fixing responsive

--- src/pages/DashboardLayout.jsx
+++ src/pages/DashboardLayout.jsx
@@ -1,38 +1,35 @@
-import React, { useState } from 'react';
-import { Outlet } from 'react-router-dom';
-import Sidebar from '../components/Sidebar';
-import Header from '../components/Header';
+import React, { useState } from "react";
+import { Outlet } from "react-router-dom";
+import Sidebar from "../components/Sidebar";
+import Header from "../components/Header";
 
 const DashboardLayout = ({ onLogout, user }) => {
-    const [isSidebarOpen, setSidebarOpen] = useState(false);
-     
-    const [isCollapsed, setIsCollapsed] = useState(false);
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState(false);
 
-    return (
-        <div className="relative min-h-screen md:flex">
-            <Sidebar 
-                isSidebarOpen={isSidebarOpen} 
-                setSidebarOpen={setSidebarOpen} 
-                onLogout={onLogout} 
-               
-                isCollapsed={isCollapsed}
-                setIsCollapsed={setIsCollapsed}
-            />
+  return (
+    <div className="relative min-h-screen md:flex">
+      <Sidebar
+        isSidebarOpen={isSidebarOpen}
+        setSidebarOpen={setSidebarOpen}
+        onLogout={onLogout}
+        isCollapsed={isCollapsed}
+        setIsCollapsed={setIsCollapsed}
+      />
 
-            <div className="flex-1 flex flex-col">
-                <Header 
-                    onMenuButtonClick={() => setSidebarOpen(true)} 
-                    onLogout={onLogout} 
-                    user={user}
-                />
-                
-                <main className="flex-1 p-6 bg-gray-50 overflow-y-auto">
-                    <Outlet />
-                </main>
-            </div>
-        </div>
-    );
+      <div className="flex-1 flex flex-col">
+        <Header
+          onMenuButtonClick={() => setSidebarOpen(true)}
+          onLogout={onLogout}
+          user={user}
+        />
+
+        <main className="flex-1 p-6 bg-gray-50 overflow-y-auto">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardLayout;
-
