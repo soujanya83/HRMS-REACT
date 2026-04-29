e9243170173eb97d604c350815672eb23e190233
fraxxra <react@fraxxratech.com>
2026-04-29 15:56:53 +0530

    Fix axios client

--- src/axiosClient.js
+++ src/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: 'https://api.chrispp.au/api/v1',
 });
 
 // Request interceptor
